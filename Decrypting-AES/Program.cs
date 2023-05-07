263132b3f3ecf94a7d0afa23f3b944602662a965
Habib PourKhnLar Ahmadi <habib.pa98@gmail.com>
2023-05-07 21:03:48 +0330

    finish Decrypting method, go to test

--- Decrypting-AES/Program.cs
+++ Decrypting-AES/Program.cs
@@ -20,5 +20,30 @@ static string Decrypting(string ciphertxt)
 {
     string plaintext = null;
     // Create AesManaged    
-    return plaintext;
+    using (var aes = new AesManaged())
+    {
+        byte[] IV = new byte[16];
+        byte[] Key = {
+                    0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+                    0x09, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16,
+                };
+        // Create a decryptor
+        ICryptoTransform decryptor = aes.CreateDecryptor(Key, IV);
+        string plaintxt = "";
+        // Create the streams used for decryption. 
+        byte[] cipherBytes = Convert.FromBase64String(ciphertxt);
+        using (MemoryStream ms = new MemoryStream(cipherBytes))
+        {
+            // Create crypto stream    
+            using (CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+            {
+                // Read crypto stream    
+                using (StreamReader reader = new StreamReader(cs))
+                    plaintext = reader.ReadToEnd();
+            }
+        }
+        StringReader readers = new StringReader(plaintext);
+        plaintxt = readers.ReadLine();
+        return plaintxt;
+    }
 }
