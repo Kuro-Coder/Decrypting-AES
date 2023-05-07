14f4faff197bdc574f5444c70f0e9336ebd33b52
Habib PourKhnLar Ahmadi <habib.pa98@gmail.com>
2023-05-07 21:01:38 +0330

    create methode

--- Decrypting-AES/Program.cs
+++ Decrypting-AES/Program.cs
@@ -1,10 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Security.Cryptography;
+
 Console.WriteLine("Yo yo, wellcome to string Decrypting-AES App.");
 
 Console.Write("Type your string data:");
 
+var cryptoData = Decrypting(Console.ReadLine());
 
+Console.Write("Decrypting Data: ");
+Console.WriteLine(cryptoData);
+
+Console.WriteLine("Finish Decrypting, press any key pls.");
 
 Console.ReadKey();
 
+
+static string Decrypting(string ciphertxt)
+{
+    string plaintext = null;
+    // Create AesManaged    
+    return plaintext;
+}
