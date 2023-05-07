b6f62e52f90a206f5ce1e9d71a0d35815c57ee0d
Habib PourKhnLar Ahmadi <habib.pa98@gmail.com>
2023-05-07 21:00:26 +0330

    firest step for text

--- Decrypting-AES/Program.cs
+++ Decrypting-AES/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Yo yo, wellcome to string Decrypting-AES App.");
+
+Console.Write("Type your string data:");
+
+
+
+Console.ReadKey();
+
